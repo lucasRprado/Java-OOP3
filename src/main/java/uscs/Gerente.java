fc6df3c8814d0f1bf5155ad3f7f5df75b31f6373
Lucas Prado <113739174+lucasRprado@users.noreply.github.com>
2022-10-20 15:42:09 -0300

    Update Gerente.java

--- src/main/java/uscs/Gerente.java
+++ src/main/java/uscs/Gerente.java
@@ -5,17 +5,6 @@ public class Gerente extends Empregado{
     private double bonus;
     
     
-    public Gerente(){
-        
-    }
-    
-    public Gerente(String nome, String endereco, double valorHora, double horasTrab, String secretaria, double bonus){
-        super(nome, endereco, valorHora, horasTrab);
-        this.secretaria = secretaria;
-        this.bonus = bonus;
-    }
-
-    
     public String getSecretaria() {
         return secretaria;
     }
@@ -31,20 +20,29 @@ public class Gerente extends Empregado{
     public void setBonus(double bonus) {
         this.bonus = bonus;
     }
-   
+    
+        
+    public Gerente(){
+        
+    }
+    
+    public Gerente(String nome, String endereco, double valorHora, double horasTrab, String secretaria, double bonus){
+        super(nome, endereco, valorHora, horasTrab);
+        this.secretaria = secretaria;
+        this.bonus = bonus;
+    }  
     
     public void ImprimeGerente(){
         super.ImprimeEmpregado();
-        System.out.println("Secretaria: " + secretaria);
+        System.out.println("\nSecretaria: " + secretaria);
         System.out.println("Bonus salarial: R$" + bonus);
     }
-    
     
     public void CalculoSalario(){
          double salarioFinal;
          double salario = super.getHorasTrab()*super.getValorHora();
         salarioFinal = salario + bonus;
-        System.out.println("Salario final: R$" + salarioFinal);
+        System.out.println("\nSalario final: R$" + salarioFinal);
     }
     
 }
